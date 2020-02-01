16a3eb4780e9456cba06eaf9f5549b01f7f13c8a
Rendy Arya Kemal <rendyarya22@gmail.com>
2020-02-01 10:41:44 +0700

    Migrate to Phillip

--- run-phillip.py
+++ run-phillip.py
@@ -1,32 +1,36 @@
-from notAiess import notAiess, helper, Handler
+from phillip.application import Phillip
+from phillip import helper
+from phillip.handlers import Handler
 from datetime import datetime, timedelta
 import aiohttp
 import aiofiles
+import functools
 import asyncio
 import config
 from universal import db as connection
-
 from universal.classes import Hook, Status, Mode
 
 
 class Rememberer(Handler):
-    def __init__(self):
-        pass
+    MODE_ID = {"osu": 1, "taiko": 2, "catch": 3, "mania": 4}
 
     async def post_event(self, session, e, hook, embed):
         print("Posting to:", hook)
-        r = await session.post(hook, json={
-            'content': e.event_source_url,
-            'embeds': [embed]
-        })
-        
+        r = await session.post(
+            hook, json={"content": e.event_source_url, "embeds": [embed]}
+        )
         if r.status != 204:
-            if (await r.json())['code'] in (50027, 10015):
+            if (await r.json())["code"] in (50027, 10015):
                 print("Revoking:", hook)
-                await connection.query(["UPDATE hooks SET status='Cannot send hook, possibly gone -- Won't send anymore.' WHERE hook_url=?", [hook]])
+                await connection.query(
+                    [
+                        "UPDATE hooks SET status='Cannot send hook, possibly gone -- Won't send anymore.' WHERE hook_url=?",
+                        [hook],
+                    ]
+                )
         elif r.status == 429:
             print("Got rate limited; retrying")
-            await asyncio.sleep(r.header['X-RateLimit-Reset-After'])
+            await asyncio.sleep(r.header["X-RateLimit-Reset-After"])
             await self.post_event(session, e, hook, embed)
         r.close()
 
@@ -35,7 +39,7 @@ class Rememberer(Handler):
             "osu": Mode.std,
             "taiko": Mode.taiko,
             "catch": Mode.catch,
-            "mania": Mode.mania
+            "mania": Mode.mania,
         }
 
         status_mapping = {
@@ -44,16 +48,18 @@ class Rememberer(Handler):
             "Disqualified": Status.disqualify,
             "Popped": Status.pop,
             "Ranked": Status.ranked,
-            "Loved": Status.loved
+            "Loved": Status.loved,
         }
 
         print("Event received")
         print("Updating last count")
-        async with aiofiles.open('last.txt', mode='w') as f:
-            await f.write((e.time + timedelta(seconds=1)).strftime("%Y-%m-%dT%H:%M:%S+00:00"))
+        async with aiofiles.open("last.txt", mode="w") as f:
+            await f.write(
+                (e.time + timedelta(seconds=1)).strftime("%Y-%m-%dT%H:%M:%S+00:00")
+            )
 
         print("Generating embed")
-        embed = await helper.gen_embed(e)
+        embed = await helper.gen_embed(e, self.app)
         targets = []
         print("Querying targets")
         hooks = await connection.query("SELECT * FROM hooks")
@@ -61,7 +67,11 @@ class Rememberer(Handler):
         for hook in hooks:
             hook_cls = Hook(hook)
             for mode in e.gamemodes:
-                if mode_mapping[mode] in hook_cls.mode and status_mapping[e.event_type] in hook_cls.push_status and not hook_cls.status:
+                if (
+                    mode_mapping[mode] in hook_cls.mode
+                    and status_mapping[e.event_type] in hook_cls.push_status
+                    and not hook_cls.status
+                ):
                     targets.append(hook_cls.hook_url)
 
         targets = list(set(targets))
@@ -72,21 +82,17 @@ class Rememberer(Handler):
 
 def main():
     try:
-        with open('last.txt', 'r') as f:
-            dt = datetime.strptime(f.read().strip(),
-                                   '%Y-%m-%dT%H:%M:%S+00:00')
+        with open("last.txt", "r") as f:
+            dt = datetime.strptime(f.read().strip(), "%Y-%m-%dT%H:%M:%S+00:00")
     except:
-        open('last.txt', 'w').close()
+        open("last.txt", "w").close()
         dt = datetime(1990, 1, 1)
 
-    handlers = [
-        Rememberer()
-    ]
+    handlers = [Rememberer()]
 
-    nA = notAiess(config.osu_token,
-                  dt, handlers=handlers)
-    nA.disable_user = True
-    nA.run()
+    ph = Phillip(config.osu_token, dt, handlers=handlers)
+    ph.disable_user = True
+    ph.run()
 
 
 if __name__ == "__main__":
